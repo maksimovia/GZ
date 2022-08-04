40283a10678994da501d4e1210f669c1fc26b57d
st-igoriy <110552508+st-igoriy@users.noreply.github.com>
2022-08-04 10:19:02 +0300

    1.1

---
+++ cotel.py
@@ -0,0 +1,168 @@
+import numpy as n
+from scipy.optimize import root
+# import Properties as propcoolprop
+import pandas as pd
+
+import json, CoolProp.CoolProp as CP
+
+#RP = mat_properties.init_REFPROP(r'C:\Program Files (x86)\REFPROP')
+
+class heatex:
+    def __init__(self,stream11,stream12,stream21,stream22,KPD,calctolerance,
+                 gas0,gas1,water,calcmethod,gas_streams0,water_streams0,gas_streams,water_streams):
+        
+        self.KPD=KPD
+        self.gas_streams0=gas_streams0
+        self.water_streams0=water_streams0
+        self.gas_streams=gas_streams
+        self.water_streams=water_streams
+        self.calcmethod=calcmethod
+        self.calctolerance=calctolerance
+        self.gas=gas1
+        self.water=water
+        self.gas0=gas0
+        self.H011 = gas_streams0.at[stream11,'H']
+        self.H012 = gas_streams0.at[stream12,'H']
+        self.H021 = water_streams0.at[stream21,'H']
+        self.H022 = water_streams0.at[stream22,'H']
+        self.G01  = gas_streams0.at[stream11,'G']
+        self.G02  = water_streams0.at[stream21,'G']
+        self.P01  = gas_streams0.at[stream11,'P']
+        self.P021 = water_streams0.at[stream21,'P']
+        self.P022 = water_streams0.at[stream22,'P']
+        self.H11  = gas_streams.at[stream11,'H']
+        self.H21  = water_streams.at[stream21,'H']
+        self.G1   = gas_streams.at[stream11,'G']
+        self.G2   = water_streams.at[stream21,'G']
+        self.P1   = gas_streams.at[stream11,'P']
+        self.P21  = water_streams.at[stream21,'P']
+        
+    def calc(self):
+        Q0     = self.G01*(self.H011-self.H012)*self.KPD
+        T011   = self.gas0.p_h(self.P01,self.H011)['T']
+        T012   = self.gas0.p_h(self.P01,self.H012)['T']
+        T021   = self.water.p_h(self.P021,self.H021)['T']
+        T022   = self.water.p_h(self.P022,self.H022)['T']
+        dTmin0 = min(T011-T022,T012-T021)
+        dTmax0 = max(T011-T022,T012-T021)
+        LMTD0  = (dTmax0 - dTmin0) / (n.log(dTmax0/dTmin0))
+        T01av  = (T011+T012)/2
+        T02av  = (T021+T022)/2
+        P02av  = (self.P021+self.P022)/2
+        lambda01av = self.gas0.p_t(self.P01,T01av)['L']
+        Pr01av = self.gas0.p_t(self.P01,T01av)['Prandtl']
+        nu01av = self.gas0.p_t(self.P01,T01av)['nu']
+        ro01av = self.gas0.p_t(self.P01,T01av)['rho']
+        ro02av = self.water.p_t(P02av,T02av)['rho']
+        ro021  = self.water.p_q(self.P021,1)['rho']        
+        ro21   = self.water.p_q(self.P21,1)['rho']
+        ddp    = (ro21/ro021)*((self.G02/self.G2)**2)
+        P22    = self.P21 - ((self.P021-self.P022)/ddp)
+        P12 = self.P1
+        def T12sved(T12):           
+            T11 = self.gas.p_h(self.P1,self.H11)['T']
+            T21 = self.water.p_h(self.P21,self.H21)['T']
+            H12 = self.gas.p_t(self.P1,T12)['h']
+            Q = self.G1*(self.H11-H12)*self.KPD
+            H22 = self.H21 + (Q/self.G2)
+            T22 = self.water.p_h(P22,H22)['T']
+            dTmin= min(T11-T22,T12-T21)
+            dTmax= max(T11-T22,T12-T21)
+            if dTmin<0 or dTmax<0 or dTmin==dTmax:   ##от зацикливания
+                LMTD = (dTmax+dTmin)/2
+            else:
+                LMTD = (dTmax - dTmin) / (n.log(dTmax/dTmin))            
+            dt = LMTD0/LMTD
+            T1av = (T11+T12)/2
+            lambda1av = self.gas.p_t(self.P1,T1av)['L']
+            Pr1av = self.gas.p_t(self.P1,T1av)['Prandtl']
+            nu1av = self.gas.p_t(self.P1,T1av)['nu']
+            ro1av = self.gas.p_t(self.P1,T1av)['rho']
+            kk = (lambda01av/lambda1av)*((Pr01av/Pr1av)**0.33)*(((self.G01/self.G1)*(ro1av/ro01av)*(nu1av/nu01av))**0.685)
+            Qq = Q0 / (kk*dt)
+            return ((Q-Qq)/Q)
+        sol = root(T12sved, T012, method=self.calcmethod, tol=self.calctolerance)
+        T12=float(sol.x)
+        H12 = self.gas.p_t(self.P1,T12)['h']
+        Q = self.G1*(self.H11-H12)*self.KPD
+        H22 = self.H21 + (Q/self.G2)
+        T22 = self.water.p_h(P22,H22)['T']
+        return [T12,P12,H12,self.G1,T22,P22,H22,self.G2,Q]
+    
+    
+class vapor:
+    def __init__(self, stream11, stream12,stream21,stream22,KPD,calctolerance,gas,gas0,water,calcmethod,gas_streams0,water_streams0,gas_streams,water_streams):
+  
+        self.KPD=KPD
+        self.gas_streams0=gas_streams0
+        self.water_streams0=water_streams0
+        self.gas_streams=gas_streams
+        self.water_streams=water_streams
+        self.calcmethod=calcmethod
+        self.calctolerance=calctolerance
+        self.gas=gas
+        self.water=water
+        self.gas0=gas0
+        self.H011 = gas_streams0.at[stream11,'H']
+        self.H012 = gas_streams0.at[stream12,'H']
+        self.H021 = water_streams0.at[stream21,'H']
+        self.H022 = water_streams0.at[stream22,'H']
+        self.G01  = gas_streams0.at[stream11,'G']
+        self.G02  = water_streams0.at[stream21,'G']
+        self.P01  = gas_streams0.at[stream11,'P']
+        self.P02  = water_streams0.at[stream21,'P']
+        self.H11  = gas_streams.at[stream11,'H']
+        self.H21  = water_streams.at[stream21,'H']
+        self.G1   = gas_streams.at[stream11,'G']
+        self.P1   = gas_streams.at[stream11,'P']
+        self.P2   = water_streams.at[stream21,'P']
+    def calc(self):
+        Q0     = self.G01*(self.H011-self.H012)*self.KPD
+        T011   = self.gas0.p_h(self.P01,self.H011)['T']
+        T012   = self.gas0.p_h(self.P01,self.H012)['T']
+        T021   = self.water.p_h(self.P02,self.H021)['T']
+        T022   = self.water.p_h(self.P02,self.H022)['T']
+        T0np   = self.water.p_q(self.P2,1)['T']        
+        dTmin0 = min(T011-T0np,T012-T0np)
+        dTmax0 = max(T011-T0np,T012-T0np)
+        LMTD0  = (dTmax0 - dTmin0) / (n.log(dTmax0/dTmin0))
+        T01av  = (T011+T012)/2
+        lambda01av = self.gas0.p_t(self.P01,T01av)['L']
+        Pr01av = self.gas0.p_t(self.P01,T01av)['Prandtl']
+        nu01av = self.gas0.p_t(self.P01,T01av)['nu']
+        ro01av = self.gas0.p_t(self.P01,T01av)['rho']
+                
+        def T12sved(T12):
+            T11 = self.gas.p_h(self.P1,self.H11)['T']
+            T21 = self.water.p_h(self.P2,self.H21)['T']
+            H12 = self.gas.p_t(self.P1,T12)['h']
+            Q = self.G1*(self.H11-H12)*self.KPD
+            H22 = self.water.p_q(self.P2,1)['h']  
+            T22 = self.water.p_h(self.P2,H22)['T']
+            G2 = Q/(H22-self.H21)
+            dTmin= min(T11-T22,T12-T21)
+            dTmax= max(T11-T22,T12-T21)
+            if dTmin<0 or dTmax<0 or dTmin==dTmax:   ##от зацикливания
+                LMTD = (dTmax+dTmin)/2
+            else:
+                LMTD = (dTmax - dTmin) / (n.log(dTmax/dTmin))            
+            dt = LMTD0/LMTD
+            T1av = (T11+T12)/2
+            lambda1av = self.gas.p_t(self.P1,T1av)['L']
+            Pr1av = self.gas.p_t(self.P1,T1av)['Prandtl']
+            nu1av = self.gas.p_t(self.P1,T1av)['nu']
+            ro1av = self.gas.p_t(self.P1,T1av)['rho']
+            kk = (lambda01av/lambda1av)*((Pr01av/Pr1av)**0.33)*(((self.G01/self.G1)*(ro1av/ro01av)*(nu1av/nu01av))**0.685)
+            Qq = Q0 / (kk*dt)
+            return ((Q-Qq)/Q)
+        sol = root(T12sved, T012, method=self.calcmethod, tol=self.calctolerance)
+        T12=float(sol.x)
+        H12 = self.gas.p_t(self.P1,T12)['h']
+        Q = self.G1*(self.H11-H12)*self.KPD
+        H22 = self.water.p_q(self.P2,1)['h']
+        T22 = self.water.p_h(self.P2,H22)['T']
+        G2 = Q/(H22-self.H21)
+        return [T12,self.P1,H12,self.G1,T22,self.P2,H22,G2,Q]
+
+
+
---
+++ mat_properties.py
@@ -0,0 +1,145 @@
+from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary
+import os
+
+def init_REFPROP(path_to_refplot):
+    try:
+        RP = REFPROPFunctionLibrary(os.environ['RPPREFIX'])
+        #RP = REFPROPFunctionLibrary('/'.join([path_to_refplot, 'REFPROP.dll']))
+    except ValueError:
+        print('Не удалось загрузить библиотеку REFPROP, проверьте путь к папке')
+        return None
+    else:
+        RP.SETPATHdll(path_to_refplot)
+        return RP
+
+
+def REFPROP_h_s(h, s, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'HS', 'P;T;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, h, s, fraction)
+    res = dict()
+    res['p'] = prop.Output[0]
+    res['T'] = prop.Output[1]-273.15
+    res['rho'] = prop.Output[2]
+    res['cp'] = prop.Output[3]
+    res['cv'] = prop.Output[4]
+    fraction_local=list(fraction)
+    if fraction_local[3]>0.05:
+        fraction_local[2]=fraction_local[2]+fraction_local[3]-0.05
+        fraction_local[3]=0.05
+    prop1 = RP.REFPROPdll(gas, 'HS', 'P;T;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, h, s, fraction_local)
+    res['nu'] = prop1.Output[5] / 100.
+    res['Prandtl'] = prop1.Output[6] 
+    res['L'] = prop1.Output[7] 
+    return res
+
+
+def REFPROP_p_t(p, t, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'PT', 'H;S;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, t, fraction)
+    res = dict()
+    res['h'] = prop.Output[0]/1000
+    res['s'] = prop.Output[1]/1000
+    res['rho'] = prop.Output[2]
+    res['cp'] = prop.Output[3]
+    res['cv'] = prop.Output[4]
+    fraction_local=list(fraction)
+    if fraction_local[3]>0.05:
+        fraction_local[2]=fraction_local[2]+fraction_local[3]-0.05
+        fraction_local[3]=0.05
+    prop1 = RP.REFPROPdll(gas, 'PT', 'H;S;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, t, fraction_local)
+    res['nu'] = prop1.Output[5] / 100.
+    res['Prandtl'] = prop1.Output[6] 
+    res['L'] = prop1.Output[7] 
+    return res
+
+
+def REFPROP_p_h(p, h, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'PH', 'T;S;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, h, fraction)
+    res = dict()
+    res['T'] = prop.Output[0]-273.15
+    res['s'] = prop.Output[1]/1000
+    res['rho'] = prop.Output[2]
+    res['cp'] = prop.Output[3]
+    res['cv'] = prop.Output[4]
+    fraction_local=list(fraction)
+    if fraction_local[3]>0.05:
+        fraction_local[2]=fraction_local[2]+fraction_local[3]-0.05
+        fraction_local[3]=0.05
+    k = prop.Output[5]
+    prop1 = RP.REFPROPdll(gas, 'PH', 'T;S;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, h, fraction_local)
+    res['nu'] = prop1.Output[5] / 100.
+    res['Prandtl'] = prop1.Output[6] 
+    res['L'] = prop1.Output[7] 
+    return res
+
+
+def REFPROP_p_s(p, s, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'PS', 'T;H;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, s, fraction)
+    res = dict()
+    res['T'] = prop.Output[0]-273.15
+    res['h'] = prop.Output[1]/1000
+    res['rho'] = prop.Output[2]
+    res['cp'] = prop.Output[3]
+    res['cv'] = prop.Output[4]
+    fraction_local=list(fraction)
+    if fraction_local[3]>0.05:
+        fraction_local[2]=fraction_local[2]+fraction_local[3]-0.05
+        fraction_local[3]=0.05
+    prop1 = RP.REFPROPdll(gas, 'PS', 'T;H;D;CV;CP;KV;Prandtl;TCX;VIS', 21, 0, 0, p, s, fraction_local)
+    res['nu'] = prop1.Output[5] / 100.
+    res['Prandtl'] = prop1.Output[6] 
+    res['L'] = prop1.Output[7] 
+    return res
+
+def REFPROP_p_q(p, q, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'PQ', 'T;D;H', 21, 0, 0, p, q,fraction)
+    res = dict()
+    res['T'] = prop.Output[0]-273.15
+    res['rho'] = prop.Output[1]
+    res['h'] = prop.Output[2]/1000
+    return res
+
+def REFPROP_t_q(t, q, gas,fraction, RP):
+    prop = RP.REFPROPdll(gas, 'PQ', 'P;D;H', 21, 0, 0, t, q,fraction)
+    res = dict()
+    res['P'] = prop.Output[0]/1e6
+    res['rho'] = prop.Output[1]
+    res['h'] = prop.Output[2]/1000
+    return res
+
+
+class Materials_prop:
+    def __init__(self, mat_name, fraction, hs_func, pt_func, ph_func, ps_func,pq_func, tq_func, *args, **kwargs):
+        self.__mat_name = mat_name
+        self.__fraction = fraction
+        self.__hs_func = hs_func
+        self.__pt_func = pt_func
+        self.__ph_func = ph_func
+        self.__ps_func = ps_func
+        self.__pq_func = pq_func
+        self.__tq_func = tq_func
+        self.__params = dict()
+        for key, val in kwargs.items():
+            self.__params[key] = val
+
+    def h_s(self, h, s):
+        return self.__hs_func(h=h*1000, s=s*1000, gas=self.__mat_name,fraction=self.__fraction,  **self.__params)
+
+    def p_t(self, p, t):
+        return self.__pt_func(p=p*1e6, t=t+273.15, gas=self.__mat_name,fraction=self.__fraction, **self.__params)
+
+    def p_h(self, p, h):
+        return self.__ph_func(p=p*1e6, h=h*1000, gas=self.__mat_name,fraction=self.__fraction, **self.__params)
+
+    def p_s(self, p, s):
+        return self.__ps_func(p=p*1e6, s=s*1000, gas=self.__mat_name,fraction=self.__fraction, **self.__params)
+    
+    def p_q(self, p, q):
+        return self.__pq_func(p=p*1e6, q=q, gas=self.__mat_name,fraction=self.__fraction, **self.__params)
+    
+    def t_q(self, t, q):
+        return self.__pq_func(t=t+273.15, q=q, gas=self.__mat_name,fraction=self.__fraction, **self.__params)
+    
+
+# RP = init_REFPROP(r'C:\Program Files (x86)\MINI-REFPROP')
+# print(REFPROP_p_h_s(502390, 2110.4, 'CO2', RP))
+# mt = Materials_prop('CO2', REFPROP_p_t, REFPROP_p_h, REFPROP_h_s, RP=RP)
+# print(mt.h_s(502390, 2110.4))
