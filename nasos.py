0bfd6f6818b7c2a6a85f739fd77ea281f3a3728c
st-igoriy <st.igoriy@gmail.com>
2022-08-04 14:18:49 +0300

    Файлы от 04.08

--- nasos.py
+++ nasos.py
@@ -1,21 +1,3 @@
-import mat_properties as prop
-import pandas as pd
-
-
-RP = prop.init_REFPROP(r'C:\Program Files (x86)\REFPROP')
-
-
-
-water=prop.Materials_prop('water',[1.0,0,0,0,0], prop.REFPROP_h_s,
-                                              prop.REFPROP_p_t,
-                                              prop.REFPROP_p_h,
-                                              prop.REFPROP_p_s,
-                                              prop.REFPROP_p_q,
-                                              prop.REFPROP_t_q,
-                                              RP=RP)
-
-
-
 class nasos:
     def __init__(self,stream11,stream12,water,KPDnasos,water_streams):
         self.KPDnasos = KPDnasos
@@ -24,7 +6,6 @@ class nasos:
         self.T0 = water_streams.at[stream11,'T']
         self.G  = water_streams.at[stream11,'G']
         self.water=water
-        
     def calc(self):
         self.s1teor = self.water.p_t(self.P0,self.T0)['s']
         self.h1teornasos = self.water.p_s(self.P1,self.s1teor)['h']
