ae2d6fe4ad8dc789c8d70e0a520033c2e9c8fc44
maximka-maker <oparinmv@mail.ru>
2022-08-04 10:52:33 +0300

    Update cotel.py

--- cotel.py
+++ cotel.py
@@ -165,5 +165,11 @@ class vapor:
         return [T12,self.P1,H12,self.G1,T22,self.P2,H22,G2,Q]
 
 
-# print(1)
-print('Igor smotri che mogy')
+    
+    
+    
+    print('Igor smotri che mogy')
+    
+    
+    
+    print('Test')
